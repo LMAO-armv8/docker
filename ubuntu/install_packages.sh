8d64f80d8b0b328576bea8f9b911e8f32e695b52
Chaitanya_Yeranagula <chaitanya.yeranagula@finmkt.io>
2024-09-08 17:27:44 +0530

    use docker python3.12

--- ubuntu/install_packages.sh
+++ ubuntu/install_packages.sh
@@ -7,15 +7,5 @@ apt-get update
 apt-get -y upgrade
 apt-get build-dep linux linux-image-$(uname -r)
 apt-get install -y build-essential software-properties-common
-add-apt-repository ppa:deadsnakes/ppa
-apt update
-apt install python3.11
-ln -sf /usr/local/bin/python3.11 /usr/local/bin/python
-ln -sf /usr/local/bin/python3.11 /usr/local/bin/python3
-update-alternatives --install /usr/bin/python python /usr/bin/python3.11 1
-update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 1
-update-alternatives --config python
-update-alternatives --config python3
 python --version
-python3 --version
 
