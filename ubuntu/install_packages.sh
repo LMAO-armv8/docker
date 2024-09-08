f4486e6aa5ed1b7a952e9bd5694cf2b81f76f09d
Chaitanya_Yeranagula <chaitanya.yeranagula@finmkt.io>
2024-09-08 16:16:12 +0530

    cleanup and use python3.11

--- ubuntu/install_packages.sh
+++ ubuntu/install_packages.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
 sed -i 's/# \(.*multiverse$\)/\1/g' /etc/apt/sources.list
-echo "deb-src http://archive.ubuntu.com/ubuntu disco main" | tee -a /etc/apt/sources.list
-echo "deb-src http://archive.ubuntu.com/ubuntu disco-updates main" | tee -a /etc/apt/sources.list
+echo "deb-src http://old-releases.ubuntu.com/ubuntu disco main" | tee -a /etc/apt/sources.list
+echo "deb-src http://old-releases.ubuntu.com/ubuntu disco-updates main" | tee -a /etc/apt/sources.list
 apt-get update
 apt-get -y upgrade
 apt-get build-dep linux linux-image-$(uname -r)
-apt-get install -y build-essential
-apt-get install -y byobu curl git htop man unzip vim wget tmate neofetch
-apt-get install -y git aria2 openjdk-8-jdk
-apt-get install -y libncurses-dev gawk flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf llvm clang
-apt-get install -y python3 python-is-python3 pip
+apt-get install -y build-essential software-properties-common
+add-apt-repository ppa:deadsnakes/ppa
+apt update
+apt install python3.11
+ln -sf /usr/local/bin/python3.11 /usr/local/bin/python
+ln -sf /usr/local/bin/python3.11 /usr/local/bin/python3
+update-alternatives --install /usr/bin/python python /usr/bin/python3.11 1
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 1
+update-alternatives --config python
+update-alternatives --config python3
+python --version
+python3 --version
 
