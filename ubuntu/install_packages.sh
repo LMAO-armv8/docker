0e32cc3e7e5365927519de68bd28ef0cb3abe377
Chaitanya_Yeranagula <chaitanya.yeranagula@finmkt.io>
2024-09-08 18:57:49 +0530

    fix docker

--- ubuntu/install_packages.sh
+++ ubuntu/install_packages.sh
@@ -7,5 +7,30 @@ apt-get update
 apt-get -y upgrade
 apt-get build-dep linux linux-image-$(uname -r)
 apt-get install -y build-essential software-properties-common
-python --version
+apt-get install python
+apt-get install python3.12-venv
 
+python3.12 -m venv myenv
+source myenv/bin/activate
+
+
+
+
+# sudo apt update
+# sudo apt install apt-transport-https ca-certificates curl software-properties-common
+
+
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+
+# echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# sudo apt update
+# sudo apt install docker-ce docker-ce-cli containerd.io
+
+# sudo systemctl status docker
+# docker --version
+
+# sudo groupadd docker
+# sudo usermod -aG docker $USER
+# newgrp docker
